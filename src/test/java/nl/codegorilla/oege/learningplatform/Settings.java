c8e5c4cba69dc4380a26eca8c43d8af9e147f272
OHiddema <oegehiddema@gmail.com>
2023-04-15 07:16:58 +0200

    Add unversioned files.

---
+++ src/test/java/nl/codegorilla/oege/learningplatform/Settings.java
@@ -0,0 +1,29 @@
+package nl.codegorilla.oege.learningplatform;
+
+public class Settings {
+    private int maxPatternLength;
+    private int maxGap;
+    private double minSupRel;
+
+    // default constructor, necessary for Jackson library!
+    public Settings() {
+    }
+
+    public Settings(int maxPatternLength, int maxGap, double minSupRel) {
+        this.maxPatternLength = maxPatternLength;
+        this.maxGap = maxGap;
+        this.minSupRel = minSupRel;
+    }
+
+    public int getMaxPatternLength() {
+        return maxPatternLength;
+    }
+
+    public int getMaxGap() {
+        return maxGap;
+    }
+
+    public double getMinSupRel() {
+        return minSupRel;
+    }
+}
---
+++ src/test/java/nl/codegorilla/oege/learningplatform/Target.java
@@ -0,0 +1,32 @@
+package nl.codegorilla.oege.learningplatform;
+
+import java.util.List;
+
+class Target {
+    String studentNr;
+    String targetCode;
+    List<String> steps;
+
+    // Jackson library requires that getters are defined for ALL fields!
+    public String getStudentNr() {
+        return studentNr;
+    }
+
+    public String getTargetCode() {
+        return targetCode;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    // default constructor, necessary for Jackson library!
+    public Target() {
+    }
+
+    public Target(String studentNr, String targetCode, List<String> steps) {
+        this.studentNr = studentNr;
+        this.targetCode = targetCode;
+        this.steps = steps;
+    }
+}
